0b9fdd374efb82dd2a5509ca191ba05cabe577cf
Go Koide <koide-g@acaric.co.jp>
2017-10-16 17:39:06 +0900

    fix test string

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 // const regexp = /^[^\x00-\x1F "#\$%\&'\*,\.\/:;<=>\?\[\\\]\^`\{\|\}~\x7F](?:[^\x00-\x1F"#\$%\&'\*,\/:;<=>\?\[\\\]\^`\{\|\}~\x7F]*[^\x00-\x1F "#\$%\&'\*,\/:;<=>\?\[\\\]\^`\{\|\}~\x7F])?$/
 
 const allow_start_regexp = /^[^\x00-\x1F "#\$%\&'\*,\.\/:;<=>\?\[\\\]\^`\{\|\}~\x7F]/
+const allow_start_regexp_file = /^[^\x00-\x1F 　"#\$%\&'\*,\.\/:;<=>\?\[\\\]\^`\{\|\}~\x7F]/
 const allow_anywhere_regexp = /[^\x00-\x1F"#\$%\&'\*,\/:;<=>\?\[\\\]\^`\{\|\}~\x7F]/
+const allow_anywhere_regexp_file = /[^\x00-\x1F 　"#\$%\&'\*,\/:;<=>\?\[\\\]\^`\{\|\}~\x7F]/
 const allow_last_regexp = /[^\x00-\x1F "#\$%\&'\*,\/:;<=>\?\[\\\]\^`\{\|\}~\x7F]$/
+const allow_last_regexp_file = /[^\x00-\x1F 　"#\$%\&'\*,\/:;<=>\?\[\\\]\^`\{\|\}~\x7F]$/
 const test = require('tape')
 
 // var safe_pattern = function (pattern) {
 //   return !!regexp.test(pattern)
 // }
 
-// 1.先頭の禁止文字をはじく
+// 1.先頭の禁止文字をはじく Project
 // -> ドットとスペースは先頭に使えません
 var allow_start_pattern = function (pattern) {
   return !!allow_start_regexp.test(pattern)
 }
+// 1.先頭の禁止文字をはじく File/Folder 全角スペースもはじく
+var allow_start_pattern_file = function (pattern) {
+  return !!allow_start_regexp_file.test(pattern)
+}
 
-// 2.禁止文字をはじく
+// 2.禁止文字をはじく Project
 var allow_anywhere_pattern = function (pattern) {
   return !!allow_anywhere_regexp.test(pattern)
 }
-// 3.末尾の禁止文字をはじく
+// 2.禁止文字をはじく File/Folder スペース類もはじく
+var allow_anywhere_pattern_file = function (pattern) {
+  return !!allow_anywhere_regexp_file.test(pattern)
+}
+// 3.末尾の禁止文字をはじく Project
 // -> スペースは末尾に使えません
 var allow_last_pattern = function (pattern) {
   return !!allow_last_regexp.test(pattern)
+}
+// 3.末尾の禁止文字をはじく File/Folder 全角スペースもはじく
+var allow_last_pattern_file = function (pattern) {
+  return !!allow_last_regexp_file.test(pattern)
 }
 
 
 test('forbidden_start: 先頭禁止パタン', function (t) {
   t.equal(allow_start_pattern(".dot"), false)
   t.equal(allow_start_pattern("\x20dot"), false)
-  t.equal(allow_start_pattern("　dot"), true) //jsでは許容
+  t.equal(allow_start_pattern("　dot"), true) //jsでは許容 Project
+  t.equal(allow_start_pattern_file("　dot"), false) //js File/Folderでは許容しない
   t.equal(allow_start_pattern("\tdot"), false)
   t.equal(allow_start_pattern("\rdot"), false)
   t.equal(allow_start_pattern("\ndot"), false)
@@ -41,7 +57,8 @@ test('forbidden_start: 先頭禁止パタン', function (t) {
 
 test('forbidden_last: 文末禁止パタン', function (t) {
   t.equal(allow_last_pattern("dot\x20"), false)
-  t.equal(allow_last_pattern("dot　"), true) //jsでは許容
+  t.equal(allow_last_pattern("dot　"), true) //jsでは許容 Project
+  t.equal(allow_last_pattern_file("dot　"), false) //js File/Folderでは許容しない
   t.equal(allow_last_pattern("dot\t"), false)
   t.equal(allow_last_pattern("dot\r"), false)
   t.equal(allow_last_pattern("dot\n"), false)
@@ -96,14 +113,22 @@ test('greek ギリシャ語はOK', function (t) {
   t.end()
 })
 
-test('space 行中半角スペース 全角スペースはOK', function (t) {
-  t.equal(allow_anywhere_pattern("test test"), true)
-  t.equal(allow_anywhere_pattern("こんに　ちは"), true)
+test('Project: space 文中半角スペース 全角スペースはOK', function (t) {
+  t.equal(allow_anywhere_pattern(" "), true)
+  t.equal(allow_anywhere_pattern("　"), true)
 
   t.end()
 })
 
-test('meta_char 制御文字はOK', function (t) {
+//
+test('File/Folder: space 文中半角スペース 全角スペースはNG', function (t) {
+  t.equal(allow_anywhere_pattern_file(" "), false)
+  t.equal(allow_anywhere_pattern_file("　"), false)
+
+  t.end()
+})
+
+test('meta_char 制御文字はNG', function (t) {
   // \x00-\x1F\x7F
   t.equal(allow_anywhere_pattern("\x00"), false)
   t.equal(allow_anywhere_pattern("\x1F"), false)
